f34d531302149dd2b0f387437df1e09d046139b7
kkkml <kml336@qq.com>
2019-03-15 09:05:49 +0800

    第一次提交

---
+++ src/main/java/bean/Class.java
@@ -0,0 +1,13 @@
+package bean;
+
+public class Class {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
